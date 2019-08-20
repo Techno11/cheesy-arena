c88e8c4f717ad7ffd78ce6eac15b31885e01276c
Soren Zaiser <soren.zaiser@gmail.com>
2019-08-19 23:25:36 -0400

    Refactor to my repo after merging

--- web/setup_awards.go
+++ web/setup_awards.go
@@ -6,8 +6,8 @@
 package web
 
 import (
-	"github.com/Team254/cheesy-arena/model"
-	"github.com/Team254/cheesy-arena/tournament"
+	"github.com/Techno11/cheesy-arena/model"
+	"github.com/Techno11/cheesy-arena/tournament"
 	"net/http"
 	"strconv"
 )
--- web/setup_awards_test.go
+++ web/setup_awards_test.go
@@ -4,7 +4,7 @@
 package web
 
 import (
-	"github.com/Team254/cheesy-arena/model"
+	"github.com/Techno11/cheesy-arena/model"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
